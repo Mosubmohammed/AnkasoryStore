696dbbb9d6cbfedc674706762ba2279229665cfc
Mosubmohammed <mosup.050@gmail.com>
2024-11-19 20:52:36 +0300

    16

--- Cart/cart.py
+++ Cart/cart.py
@@ -7,7 +7,7 @@ class Cart():
         #! get the current session key if it exists
         cart=self.session.get("session_key")
         
-        #! if the  useris new ,no session key, create one
+        #! if the  useris new ,no session key, create one   
         
         if 'session_key' not in request.session:
             cart=self.session['session_key']={}
--- Cart/views.py
+++ Cart/views.py
@@ -14,6 +14,7 @@ def cart_summary(request):
 def cart_add(request):
     cart=Cart(request)
     if request.POST.get('action') == 'post':
+        print()
         product_id=int(request.POST.get('product_id'))
         product_qty=int(request.POST.get('product_qty'))
         product=get_object_or_404(Product,id=product_id)
